4dac1e211afce0a90caf7b0495dbe1e702f8f7cf
ArcNetic <arcneticyt@gmail.com>
2026-08-16 12:21:49 +0530

    Added lexer test cases

---
+++ tests/lexer/comments.c
@@ -0,0 +1,14 @@
+// Single-line comment
+int x = 10; /* inline comment */
+
+/*
+   Multi-line comment
+   should be ignored.
+*/
+
+int y = 20;
+
+/* first */
+/* second */
+
+int z = 30;
---
+++ tests/lexer/errors.c
@@ -0,0 +1,10 @@
+int x = 10 @ 20;
+float y = 3.14 # 2;
+
+char a = 'ab';
+char b = 'x;
+
+int c = "hello
+int d = 20;
+
+/* unterminated comment
