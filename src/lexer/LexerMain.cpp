b9bc95a31e104576dd50ba5dece03b1edd044df8
ArcNetic <arcneticyt@gmail.com>
2026-08-16 12:16:03 +0530

    Added command line driver

---
+++ src/lexer/LexerMain.cpp
@@ -0,0 +1,51 @@
+#include <fstream>
+#include <iostream>
+
+#include "Lexer.hpp"
+
+int main(int argc, char* argv[])
+{
+    if (argc != 2)
+    {
+        std::cerr << "Usage: " << argv[0] << " <source-file>\n";
+        return 1;
+    }
+
+    std::ifstream input(argv[1]);
+
+    if (!input)
+    {
+        std::cerr << "Error opening file: " << argv[1] << '\n';
+        return 1;
+    }
+
+    Lexer lexer(&input);
+
+    while (true)
+    {
+        Token token = lexer.nextToken();
+
+        std::cout << tokenTypeToString(token.type)
+                  << " | "
+                  << token.lexeme
+                  << '\n';
+
+        if (token.type == TokenType::EndOfFile)
+            break;
+    }
+
+    if (!lexer.errors().empty())
+    {
+        std::cerr << "\nLexical Errors:\n";
+
+        for (const auto& error : lexer.errors())
+        {
+            std::cerr << error.message
+                      << " at line " << error.line
+                      << ", column " << error.column
+                      << '\n';
+        }
+    }
+
+    return lexer.errors().empty() ? 0 : 1;
+}
